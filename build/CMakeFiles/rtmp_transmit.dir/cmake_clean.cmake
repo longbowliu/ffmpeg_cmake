7e4ca9d2471cd9d7b7f38a7960a8965816b8e5b7
longbin.liu <longbin.liu@cn.innovusion.com>
2023-03-07 16:00:52 +0800

    init

---
+++ build/CMakeFiles/rtmp_transmit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtmp_transmit.dir/src/decode_video.c.o"
+  "../bin/rtmp_transmit.pdb"
+  "../bin/rtmp_transmit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rtmp_transmit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
